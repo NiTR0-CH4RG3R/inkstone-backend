8aacd72503a17e2bbfbdfe9cd8210439edc9837b
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-11-22 10:33:10 +0530

    Initial Commit

---
+++ src/test/java/com/codeprimes/inkstone/InkstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeprimes.inkstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
